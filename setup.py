1b0b9f71227895d5a2d33a694ec7e93ab236bb8d
Khaled Alkhouli <khaled.alkhouli@opensooq.com>
2025-11-23 20:29:27 +0300

    Edit .gitignore and setup files

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tfrecx",
+    version="0.1.0",
+    author="Khaled Alkhouli",
+    author_email="khaled.alkhouli03@gmail.com",
+    description="Elegant toolkit for creating TFRecords from pandas DataFrames, images, and more.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Khoulii/tfrecx",
+    packages=find_packages(exclude=["tests", "docs"]),
+    python_requires=">=3.10,<3.13",
+    install_requires=[
+        "tensorflow>=2.20.0",
+        "pandas>=2.3.3",
+        "numpy>=2.3.3",
+        "Pillow>=11.3.0"
+    ],
+    extras_require={
+        "dev": ["pytest", "black", "flake8"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries"
+    ],
+    include_package_data=True,
+)
+
